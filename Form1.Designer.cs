72100e092535154f01ea9da5aacb4f4cbea5eca8
SiAvMaAnR <samarkin200220022002@gmail.com>
2020-08-09 22:17:49 +0300

    Скорректировал размеры

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -212,7 +212,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-			this.ClientSize = new System.Drawing.Size(396, 512);
+			this.ClientSize = new System.Drawing.Size(396, 477);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.pictureBox5);
 			this.Controls.Add(this.buttonSign_In);
